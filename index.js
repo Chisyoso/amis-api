7c0d3e5349d25bf8c9851fe8dd1757ffca6063d5
CHM <132416621+Chisyoso@users.noreply.github.com>
2026-07-12 23:06:28 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); 
 const { createCanvas, loadImage, registerFont } = require("canvas");
 const fetch = require("node-fetch");
 const fs = require("fs");
